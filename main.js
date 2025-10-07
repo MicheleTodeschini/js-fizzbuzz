1825af7da45762f7e909b6b41e658972c11cb9d0
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-07 13:49:34 +0200

    add human code

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log('it works');
+
+
+/*
+- stampare numeri da 1 a 100
+- multipli di 3 stampare fizz
+- multipli 5 stampare Buzz
+- multipli 15 stampare FizzBuzz
+
+TOOLS
+
+-ciclo for
+
+*/
+
