a773dcfd9eddd0a865acdf208f8c73876e9ac50c
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-07 13:59:31 +0200

    add comments

--- main.js
+++ main.js
@@ -10,18 +10,27 @@ console.log('it works');
 TOOLS
 
 -ciclo for
+-math
+-if
+-if else
+-else 
+
 
 */
 
 for (let i = 1; i <= 100; i++){
+    // this is for numbers divisible by 3 and 5
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(`${i}FizzBuzz`);
     } else if (i % 5 === 0) {
+        // this is for numbers divisible 5
         console.log(`${i}Buzz`);
     } else if (i % 3 === 0) {
+        // this is for numbers divisible 3
         console.log(`${i}Fizz`);
         
     } else {
+        //per tutti gli altri numeri 
         console.log(i);
         
     }
