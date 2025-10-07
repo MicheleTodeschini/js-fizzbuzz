cd57e848ed86939afe6baab026e4c4e254176ee5
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-07 13:57:24 +0200

    add number multiple of 3

--- main.js
+++ main.js
@@ -15,9 +15,14 @@ TOOLS
 
 for (let i = 1; i <= 100; i++){
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log(`${i} è multiplo di 15`);
+        console.log(`${i}FizzBuzz`);
     } else if (i % 5 === 0) {
-        console.log(`${i} è multiplo di 5`);
+        console.log(`${i}Buzz`);
+    } else if (i % 3 === 0) {
+        console.log(`${i}Fizz`);
+        
+    } else {
+        console.log(i);
         
     }
 }
